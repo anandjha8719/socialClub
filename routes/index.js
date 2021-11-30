0aba7d8082eb68b0abb063687b596fc66d3d6065
anandjha8719 <anandjha8719@gmail.com>
2021-11-30 14:58:45 +0530

    posts api routes

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.use('/posts', require('./posts'));
 router.use('/comments', require('./comments'));
 
 
-router.use('/api', require('./api/v1/index'));
+router.use('/api', require('./api'));
 
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
