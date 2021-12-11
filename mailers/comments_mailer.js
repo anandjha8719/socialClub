ce874bc5032e627c8a0e221ed6b38ce5534013e9
anandjha8719 <anandjha8719@gmail.com>
2021-12-11 12:19:06 +0530

    mailing templates

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -2,13 +2,14 @@ const nodeMailer = require('../config/nodemailer');
 
 //testing how this mailer works, sending mail to the person who commented himself for now
 exports.newComment = (comment) => {
-    console.log('Inside new comment mailer', comment);
+    // console.log('Inside new comment mailer', comment);
+    let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
 
     nodeMailer.transporter.sendMail({
         from: 'anandkumarjha8719@gmail.com',
         to: comment.user.email,
-        subject: "New Comment published",
-        html: '<h1>Your comment on socialclub is published</h1>'
+        subject: "your new Comment published",
+        html: htmlString
     }, (err, info) => {
         if(err){
             console.log('error in sending mail', err);
