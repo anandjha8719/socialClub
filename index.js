5fd618fc4c64ac5f76b8a2a3c2e7c08382a1b6f6
anandjha8719 <anandjha8719@gmail.com>
2022-02-01 18:32:09 +0530

    heroku deploy 1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 // used for session cookie
