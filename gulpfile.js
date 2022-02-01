0f19f03ba1070dc72dab75ee770c570d6b063b14
anandjha8719 <anandjha8719@gmail.com>
2022-02-01 10:43:54 +0530

    gulp fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,78 @@
-const gulp = require('gulp');
+// const gulp = require('gulp');
 
-const sass = require('gulp-sass');
-const cssnano = require('gulp-cssnano');
-const cleancss = require('gulp-clean-css');
-const rev = require('gulp-rev');
-const uglify = require('gulp-uglify-es').default;
+// const sass = require('gulp-sass');
+// const cssnano = require('gulp-cssnano');
+// const cleancss = require('gulp-clean-css');
+// const rev = require('gulp-rev');
+// const uglify = require('gulp-uglify-es').default;
 
 
 
-gulp.task('css', function(done){
-    console.log('minifying css...');
-    gulp.src('./assets/scss/**/*.scss')
+// gulp.task('css', function(done){
+//     console.log('minifying css...');
+//     gulp.src('./assets/scss/**/*.scss')
+//     .pipe(sass())
+//     .pipe(cssnano())
+//     .pipe(gulp.dest('./assets/css/'));
+
+//     return gulp.src('./assets/**/*.css')
+//     .pipe(rev())
+//     .pipe(gulp.dest('./public/assets/css/'))
+//     .pipe(rev.manifest({
+//         cwd: 'public',
+//         merge: true
+//     }))
+//     .pipe(gulp.dest('./public/assets/css/'));
+//     done();
+
+// });
+
+
+// gulp.task('js', function(done){
+//     console.log('minifying js ... ... ...');
+//     gulp.src('./assets/**/*.js')
+//     .pipe(uglify())
+//     .pipe(rev())
+//     .pipe(gulp.dest('./public/assets/'))
+//     .pipe(rev.manifest({
+//         cwd: 'public',
+//         merge: true
+//     }))
+//     .pipe(gulp.dest('./public/assets/'))
+//     done();
+// });
+
+
+const { src, dest, watch, series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const prefix = require('gulp-autoprefixer');
+const minify = require('gulp-clean-css');
+const terser = require('gulp-terser');
+
+function compilescss() {
+  return src('assets/scss/*.scss') 
     .pipe(sass())
-    .pipe(cssnano())
-    .pipe(gulp.dest('./assets/css/'));
+    .pipe(prefix())
+    .pipe(minify())
+    .pipe(dest('public/assets/css')) 
+};
 
-    return gulp.src('./assets/**/*.css')
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets/css/'))
-    .pipe(rev.manifest({
-        cwd: 'public',
-        merge: true
-    }))
-    .pipe(gulp.dest('./public/assets/css/'));
-    done();
+function jsmin(){
+  return src('assets/js/*.js') 
+    .pipe(terser())
+    .pipe(dest('public/assets/js'));
+}
 
-});
+//watchtask
+function watchTask(){
+  watch('assets/scss/**/*.scss', compilescss); 
+  watch('assets/js/*.js', jsmin); 
+}
 
 
-gulp.task('js', function(done){
-    console.log('minifying js ... ... ...');
-    gulp.src('./assets/**/*.js')
-    .pipe(uglify())
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets/'))
-    .pipe(rev.manifest({
-        cwd: 'public',
-        merge: true
-    }))
-    .pipe(gulp.dest('./public/assets/'))
-    done();
-});
+// Default Gulp task 
+exports.default = series(
+  compilescss,
+  jsmin,
+  watchTask
+);
